a014caba0e9b710bfdae9e92cd518ac05ee8fdbb
fronti90 <m90braun@gmail.com>
2013-08-15 13:53:48 +0200

    adjust slim.mk for new bootanimation
    Change-Id: Ibffbd14d76fb47c123cf29def6e1f34175d9bc57

--- slim.mk
+++ slim.mk
@@ -36,10 +36,6 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-# Boot Animation
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i777
 PRODUCT_NAME := slim_i777
