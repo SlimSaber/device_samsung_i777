e584a8fbdda8e7a450e70ea3aec4042e626d2383
fronti90 <m90braun@gmail.com>
2013-12-25 20:05:21 +0100

    TWRP
    Change-Id: I267970fffd03f29da3c896c9c3994e2fbc457614

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,5 +35,18 @@ TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro/bin/arm-linux-gnueabihf-
 # assert
 TARGET_OTA_ASSERT_DEVICE := i777,SGH-I777,SGH-S959G
 
+#TWRP
+DEVICE_RESOLUTION := 480x800
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INTERNAL_STORAGE_PATH := "/emmc" #needs confirmation
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc" #needs confirmation
+TW_EXTERNAL_STORAGE_PATH := "/sdcard" #needs confirmation
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard" #needs confirmation
+TW_NO_REBOOT_BOOTLOADER := true
+TW_DEFAULT_EXTERNAL_STORAGE := false
+TW_FLASH_FROM_STORAGE := true
+TW_INCLUDE_INJECTTWRP := true
+TW_HAS_DOWNLOAD_MODE := true
+
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i777/BoardConfigVendor.mk
